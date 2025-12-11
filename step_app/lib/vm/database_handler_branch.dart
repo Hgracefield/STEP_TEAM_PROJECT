8e0037ad5988baf3719914df7cb3608a9b90e53d
Hgracefield <gracefield0814@google.com>
2025-12-11 12:29:29 +0900

    model text

---
+++ step_app/lib/vm/database_handler_branch.dart
@@ -0,0 +1,15 @@
+// int? branch_id;
+// String branch_name;
+// String branch_phone;
+// String branch_location;
+
+// class DatabaseHandlerBranch {
+//   Future<Database> initializedDB() async {
+//     String path = await getDatabasePath();
+
+//     return openDatabase(
+//       join(path, 'step.db'),
+//       onCreate:
+//     )
+//   }
+// }
